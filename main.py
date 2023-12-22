f72f90440152e2cc01d965e1bdd25bc5acd48a10
kylekce <kylekce@gmail.com>
2023-12-22 05:53:23 +0000

    Dec 22, 2023, 1:53 PM

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
-
-app = QApplication(sys.argv)
-
-window = QMainWindow()
-window.show()
-
-app.exec() # Start the event loop.
